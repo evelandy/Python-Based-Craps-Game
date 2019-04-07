0a9904387841b41c71fb44e361d70a304832d0ba
William Griffin <40611326+evelandy@users.noreply.github.com>
2019-04-07 13:59:04 -0500

    Update craps.py

--- craps.py
+++ craps.py
@@ -1,9 +1,8 @@
-"""
-PROGRAMMER: William Griffin
-DATE: Feb. 02, 2019
-ASSIGNMENT: Number #2
-ALGORITHM: This program allows users to play or quit upon start. Once started the user will roll the first round and either win, lose, or roll again,
-on second roll the user will either win or lose if they roll on the number 7. After a win or loss the user will have the option to play again.
+#!/usr/bin/env python3
+"""evelandy/W.G.
+Feb. 02 2019 2:42pm
+Craps-Game
+Python36-32 
 """
 
 import sys
